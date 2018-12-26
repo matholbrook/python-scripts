75b56606a7fa45a7a3482cc08d70cf4a4c43c16e
matholbrook <15785836+matholbrook@users.noreply.github.com>
2018-12-26 16:50:28 +0000

    self-contained script for calling API

---
+++ api-iss.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+import requests
+import json
+
+def get_api_data(apiUrl):
+	baseUrl = "http://api.open-notify.org/"	
+	apiResponse = requests.get(baseUrl + apiUrl)
+	test_status_code(apiResponse)
+	return	
+
+def test_status_code(apiResponse):
+	if apiResponse.status_code == 200:
+		print_json_data(apiResponse)
+	elif apiResponse.status_code == 400:
+		print("API reuest failed with response code " + str(apiResponse.status_code) + " (bad api request sent to endpoint server)")
+	elif apiResponse.status_code == 401:
+		print("API reuest failed with response code " + str(apiResponse.status_code) + " (endpoint server authentication error)")
+	elif apiResponse.status_code == 403:
+		print("API reuest failed with response code " + str(apiResponse.status_code) + " (access to endpoint resource is forbidden)")
+	elif apiResponse.status_code == 404:
+		print("API reuest failed with response code " + str(apiResponse.status_code) + " (resource not found on endpoint server)")
+	return
+
+def print_json_data(apiResponse):
+	parsedJSON = json.loads(apiResponse.content)
+	print(json.dumps(parsedJSON, indent=2, sort_keys=True))
+	return
+
+get_api_data(apiUrl="astros.json")
+get_api_data(apiUrl="iss-now.json")
